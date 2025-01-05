7da27193180635b7c135932fcf27dd7237e05d63
Jinson25 <am987406@gmail.com>
2025-01-05 12:35:07 -0500

    Añadir componente de menú y configurar rutas en el dashboard; actualizar dependencias y entorno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,35 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { StatsComponent } from './pages/dashboard/stats/stats.component';
+import { MenuComponent } from './pages/dashboard/menu/menu.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const titleGlobal = 'Trueques |';
+const titleGlobal = 'Tertulia |';
 
 export const routes: Routes = [
-    {
-        path: 'dashboard',
-        component: DashboardComponent,
-        title: `${titleGlobal} Dashboard`,
-        children: [
-            { path: '', component: StatsComponent,
-                title: `${titleGlobal} Estadisticas`, pathMatch: 'full' },
-        ],
-    },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    title: `${titleGlobal} Dashboard`,
+    children: [
+      {
+        path: '',
+        component: StatsComponent,
+        title: `${titleGlobal} Estadisticas`,
+        pathMatch: 'full',
+      },
+      {
+        path: 'menu',
+        component: MenuComponent,
+        pathMatch: 'full',
+        title: `${titleGlobal} Menu`,
+      },
+      {
+        path: 'auth',
+        component: LoginComponent,
+        pathMatch: 'full',
+        title: `${titleGlobal} Auth`,
+      }
+    ],
+  },
 ];
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    supabaseUrl: 'https://nfxtbfjznxgwdljkokvc.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5meHRiZmp6bnhnd2Rsamtva3ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3NjM3NzEsImV4cCI6MjA1MTMzOTc3MX0.L64wNEKUskpgAB49_SQ0mLT4zpuu4_18YYpZ-r2R8VU'
+  };
