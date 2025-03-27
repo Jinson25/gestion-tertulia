7ca7421b68dd1719e9d7a597dd6d9fd722a166cd
Jinson25 <am987406@gmail.com>
2025-03-27 16:20:03 -0500

    Añadir componentes de botones y menú diario; actualizar la página de inicio y el menú del dashboard

---
+++ src/app/components/menu-daily/menu-daily.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-daily',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-daily.component.html',
+  styleUrl: './menu-daily.component.css'
+})
+export class MenuDailyComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuDailyComponent } from '../../components/menu-daily/menu-daily.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MenuDailyComponent, CommonModule], // Removed unused import
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
